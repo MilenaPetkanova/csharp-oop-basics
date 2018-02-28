357a1a4c0e9bbf25d55ca17b15ad5a3b70714a45
Milena Petkanova <milenaslaveikova78@gmail.com>
2018-02-28 23:36:38 +0200

    75/100 - test 4 and 6 does not passed

---
+++ 0402-Inheritance-Exercises/0404-Online-Radio-Database/Song.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+public class Song
+{
+    private const string InvalidSongException = "Invalid song.";
+    private const string InvalidArtistNameException = "Artist name should be between 3 and 20 symbols.";
+    private const string InvalidSongNameException = "Song name should be between 3 and 30 symbols.";
+    private const string InvalidSongLengthException = "Invalid song length.";
+    private const string InvalidSongMinutesException = "Song minutes should be between 0 and 14.";
+    private const string InvalidSongSecondsException = "Song seconds should be between 0 and 59.";
+
+    public string ArtistName { get; set; }
+    public string SongName { get; set; }
+    public int Minutes { get; set; }
+    public int Seconds { get; set; }
+
+    public Song(string artist, string song, int minutes, int seconds)
+    {
+        this.ArtistName = artist;
+        this.SongName = song;
+        this.Minutes = minutes;
+        this.Seconds = seconds;
+        ValidateSong();
+    }
+
+    private void ValidateSong()
+    {
+        if (ArtistName.Length < 3 || ArtistName.Length > 20)
+        {
+            throw new ArgumentException(InvalidArtistNameException);
+        }
+        else if (SongName.Length < 3 || SongName.Length > 30)
+        {
+            throw new ArgumentException(InvalidSongNameException);
+        }
+        else if (Minutes > 14 && Seconds >= 0 && Seconds <= 59)
+        {
+            throw new ArgumentException(InvalidSongLengthException);
+        }
+        else if (Minutes < 0 || Minutes > 14)
+        {
+            throw new ArgumentException(InvalidSongMinutesException);
+        }
+        else if (Seconds < 0 || Seconds > 59)
+        {
+            throw new ArgumentException(InvalidSongSecondsException);
+        }
+    }
+}
---
+++ 0402-Inheritance-Exercises/0404-Online-Radio-Database/Startup.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Startup
+{
+    static void Main()
+    {
+        // 75/100 -> Test #4 (Incorrect answer), Test #6 (Incorrect answer) 
+
+        var playlist = new List<Song>();
+        var plalistSecondsLength = 0;
+
+        int songsCount = int.Parse(Console.ReadLine());
+        for (int i = 0; i < songsCount; i++)
+        {
+            try
+            {
+                var songsArgs = Console.ReadLine().Split(';');
+                var artistName = songsArgs[0];
+                var songName = songsArgs[1];
+                var songLength = songsArgs[2].Split(':');
+                int minutes = int.Parse(songLength[0]);
+                int seconds = int.Parse(songLength[1]);
+
+                var song = new Song(artistName, songName, minutes, seconds);
+                playlist.Add(song);
+                Console.WriteLine("Song added.");
+
+                plalistSecondsLength += seconds + minutes * 60;
+            }
+            catch (Exception ex)
+            {
+                if (ex is ArgumentException)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                else if (ex is FormatException || ex is IndexOutOfRangeException)
+                {
+                    Console.WriteLine("Invalid song.");
+                }
+            }
+        }
+        TimeSpan length = TimeSpan.FromSeconds(plalistSecondsLength);
+
+        Console.WriteLine($"Songs added: {playlist.Count}");
+        Console.WriteLine($"Playlist length: {length.Hours}h {length.Minutes}m {length.Seconds}s");
+    }
+}
