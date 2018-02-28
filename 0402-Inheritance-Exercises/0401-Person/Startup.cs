17f2565f1be98de8ecd1fa86f7559e77ff87ee62
Milena Petkanova <milenaslaveikova78@gmail.com>
2018-02-28 09:03:22 +0200

    Add exersice

--- 0402-Inheritance-Exercises/0401-Person/Startup.cs
+++ 0402-Inheritance-Exercises/0401-Person/Startup.cs
@@ -1,13 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
 
 class Startup
 {
     static void Main()
     {
+        string name = Console.ReadLine();
+        int age = int.Parse(Console.ReadLine());
 
+        try
+        {
+            var child = new Child(name, age);
+            Console.WriteLine(child);
+        }
+        catch (ArgumentException ea)
+        {
+            Console.WriteLine(ea.Message);
+        }
     }
 }
