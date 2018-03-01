5835ae9227309fad33136db96ce4e6d090ec3324
Milena Petkanova <milenaslaveikova78@gmail.com>
2018-03-01 13:18:54 +0200

    Add exercises

---
+++ 0502-Interfaces-And-Abstraction/0501-Define-an-Interface-IPerson/Citizen.cs
@@ -0,0 +1,15 @@
+﻿public class Citizen : IPerson
+{
+    private string name;
+    private int age;
+
+    public Citizen(string name, int age)
+    {
+        this.Name = name;
+        this.Age = age;
+    }
+
+    public int Age { get; set; }
+
+    public string Name { get; set; }
+}
---
+++ 0502-Interfaces-And-Abstraction/0501-Define-an-Interface-IPerson/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+class Startup
+{
+    static void Main()
+    {
+        string name = Console.ReadLine();
+        int age = int.Parse(Console.ReadLine());
+        IPerson person = new Citizen(name, age);
+        Console.WriteLine(person.Name);
+        Console.WriteLine(person.Age);
+    }
+}
