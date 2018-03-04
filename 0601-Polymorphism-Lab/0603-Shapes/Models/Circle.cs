40246464377288549dee67279289244784374e91
Milena Petkanova <milenaslaveikova78@gmail.com>
2018-03-04 14:29:00 +0200

    0601-Polymorphism-Lab

---
+++ 0601-Polymorphism-Lab/0603-Shapes/Models/Circle.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+public class Circle : Shape
+{
+    private double radius;
+
+    public Circle(double radius)
+    {
+        this.Radius = radius;
+    }
+
+    public double Radius
+    {
+        get => this.radius;
+        set
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(this.radius), "Width should be possitive number!");
+            }
+            radius = value;
+        }
+    }
+
+    public override double CalculatePerimeter()
+    {
+        var result = 2 * Math.PI * this.radius;
+        return result;
+    }
+
+    public override double CalculateArea()
+    {
+        var result = Math.PI * this.radius * this.radius;
+        return result;
+    }
+
+    public override string Draw()
+    {
+        return base.Draw() + "Circle";
+    }
+}
---
+++ 0601-Polymorphism-Lab/0603-Shapes/Models/Rectangle.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+public class Rectangle : Shape
+{
+    private double height;
+    private double width;
+
+    public Rectangle(double height, double width)
+    {
+        this.Height = height;
+        this.Width = width;
+    }
+
+    public double Height
+    {
+        get => this.height;
+        set
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(this.width), "Width should be possitive number!");
+            }
+            height = value;
+        }
+    }
+
+    public double Width
+    {
+        get => this.width;
+        set
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(this.width), "Width should be possitive number!");
+            }
+            width = value;
+        }
+    }
+
+    public override double CalculateArea()
+    {
+        var result = this.height * this.width;
+        return result;
+    }
+
+    public override double CalculatePerimeter()
+    {
+        var result = this.height * 2 + this.width * 2;
+        return result;
+    }
+
+    public override string Draw()
+    {
+        return base.Draw() + "Rectangle";
+    }
+}
