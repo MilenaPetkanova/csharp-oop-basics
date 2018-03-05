1ee846219ebbd89b188968d9a42951e4f7223afd
Milena Petkanova <milenaslaveikova78@gmail.com>
2018-03-05 18:47:56 +0200

    Add exercises

---
+++ 0502-Interfaces-And-Abstraction/0510-Explicit-Interfaces/Models/Cirtizen.cs
@@ -0,0 +1,29 @@
+﻿public class Citizen : IPerson, IResident
+{
+    private string name;
+    private string country;
+    private int age;
+
+    public Citizen(string name, string country, int age) 
+    {
+        this.Name = name;
+        this.Age = age;
+        this.Country = country; 
+    }
+    
+    public string Name { get; private set; }
+
+    public string Country { get; private set; }
+
+    public int Age { get; private set; }
+
+    string IResident.GetName()
+    {
+        return this.Name;
+    }
+
+    string IPerson.GetName()
+    {
+        return "Mr/Ms/Mrs " + this.Name;
+    }
+}
---
+++ 0502-Interfaces-And-Abstraction/0510-Explicit-Interfaces/Startup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Startup
+{
+    static void Main()
+    {
+        string command = string.Empty;
+        while ((command = Console.ReadLine()) != "End")
+        {
+            var info = command.Split();
+            var name = info[0];
+            var country = info[1];
+            var age = int.Parse(info[2]);
+
+            IResident resident = new Citizen(name, country, age);
+            IPerson person = new Citizen(name, country, age);
+
+            Console.WriteLine(resident.GetName());
+            Console.WriteLine(person.GetName());
+        }
+    }
+}
