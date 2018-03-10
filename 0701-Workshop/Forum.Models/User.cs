96409d6c785e694579a519c8dffbe41e86a5dbd4
Milena Petkanova <milenaslaveikova78@gmail.com>
2018-03-10 15:24:46 +0200

    Add Forum.App part 1

---
+++ 0701-Workshop/Forum.Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace Forum.Models
+{
+    public class User
+    {
+        public User(int id, string username, string password, ICollection<int> postsIds)
+        {
+            this.Id = id;
+            this.Username = username;
+            this.Password = password;
+            this.PostIds = postsIds;
+        }
+
+        public int Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public ICollection<int> PostIds { get; set; }
+    }
+}
